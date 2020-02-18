696b3bbdcf39a8f70c52556fe53abff214fbfe3a
Matthew Baum <matthew.baum8@gmail.com>
2020-02-18 11:50:51 -0500

    Abstracted JS from HTML IDs with constants

--- mash.js
+++ mash.js
@@ -22,12 +22,40 @@ const TD_CONSTANT = 0.41;
 const MAX_RESTS = 5;
 const L_PER_GAL = 3.785;
 
+/* constant definition for IDs referencing the HTML fields */
+const keys = {
+    GRAIN_MASS: 'total_grain',
+    MASH_THICKNESS: 'wg_ratio',
+    GRAIN_TEMP: 't0',
+    BATCH_VOL: 'bat_vol',
+    EVAP_RATE: 'evap_rate',
+    BOIL_LEN: 'boil_len',
+    EQUIP_LOSS: 'eq_loss',
+    TRUB_LOSS: 'trub_loss',
+    MASH_TUN_VOL: 'tun_vol',
+    MASH_RESTS: 'rests',
+    MASH_INFUSIONS: 'infusions',
+    INFUSION_COLUMNS: 'inf_cols', /* ID should be removed */
+    GRAIN_LOSS: 'grain_loss',
+    BOIL_LOSS: 'boil_loss',
+    RUNOFF_VOL: 'runoff_vol',
+    TOTAL_INFUSION_VOL: 'total_infusion',
+    SPARGE_VOL: 'sparge',
+    TOTAL_MASH_WATER_VOL: 'total_mash',
+    TOTAL_MASH_VOL: 'total_vol'
+};
+
 function set_config(key,val) {
     document.getElementById(key).value = val;
 }
 
 function get_config(key) {
     return parseFloat(document.getElementById(key).value);
+}
+
+/* get array of mash rests */
+function get_mash_rests() {
+    return document.getElementById(keys.MASH_RESTS).children;
 }
 
 function set_footer_good(row_id, is_good) {
@@ -57,10 +85,10 @@ function get_infusion_volume(grain, water, t1, t2, ti) {
 }
 
 function add_rest() {
-    var rests = document.getElementById('rests');
+    var rests = document.getElementById(keys.MASH_RESTS);
     var rest = document.createElement('tr');
     var input = document.createElement('input');
-    var rest_list = rests.children;
+    var rest_list = get_mash_rests();
     var rest_count = rest_list.length;
     
     if(rest_count >= MAX_RESTS)
@@ -77,7 +105,7 @@ function add_rest() {
         input.min = rest_list[rest_count-1].firstChild.value;
         input.value = input.min;
     } else {
-        input.min = document.getElementById('t0').value;
+        input.min = document.getElementById(keys.GRAIN_TEMP).value;
         input.value = '68.0';
     }
     
@@ -89,9 +117,9 @@ function add_rest() {
 }
 
 function add_infusion(idx) {
-    var infusions = document.getElementById('infusions');
+    var infusions = document.getElementById(keys.MASH_INFUSIONS);
     var infusion = document.createElement('tr');
-    var cols = document.getElementById('inf_cols').children.length;
+    var cols = document.getElementById(keys.INFUSION_COLUMNS).children.length;
     var i;
     
     for(i=0; i<cols; ++i)
@@ -102,10 +130,10 @@ function add_infusion(idx) {
 }
 
 function update_infusions(rest_list) {
-    var infusions = document.getElementById('infusions').children;
-    var grain = get_config('total_grain');
-    var wg_ratio = get_config('wg_ratio');
-    var t0 = get_config('t0');
+    var infusions = document.getElementById(keys.MASH_INFUSIONS).children;
+    var grain = get_config(keys.GRAIN_MASS);
+    var wg_ratio = get_config(keys.MASH_THICKNESS);
+    var t0 = get_config(keys.GRAIN_TEMP);
     var len = rest_list.length;
     var rest, t1, t2, ti, i, prev, water = 0, vol, infusion, cols;
     
@@ -130,22 +158,21 @@ function update_infusions(rest_list) {
         cols[3].innerHTML = (vol/L_PER_GAL).toFixed(2);
     }
     
-    set_footer_row('total_infusion', water);
+    set_footer_row(keys.TOTAL_INFUSION_VOL, water);
     update_sparge();
     update_total_vol();
 }
 
 function mod_rest(e) {
     var rest_num = parseInt(e.target.parentNode.getAttribute('data-rest-num'));
-    var rests = document.getElementById('rests');
-    var rest_list = rests.children;
-    var rest_count = rest_list.length;
+    var rests = get_mash_rests();
+    var rest_count = rests.length;
     
     if(rest_num < rest_count-1) {
-        update_min(rest_list, rest_num);
+        update_min(rests, rest_num);
     }
     
-    update_infusions(rest_list);
+    update_infusions(rests);
 }
 
 function update_min(rests, start) {
@@ -161,7 +188,7 @@ function update_min(rests, start) {
 }
 
 function del_rest() {
-    var rests = document.getElementById('rests');
+    var rests = document.getElementById(keys.MASH_RESTS);
     
     if(rests.getElementsByTagName('tr').length === 0)
         return;
@@ -172,7 +199,7 @@ function del_rest() {
 }
 
 function del_infusion() {
-    var infusions = document.getElementById('infusions');
+    var infusions = document.getElementById(keys.MASH_INFUSIONS);
     
     if(infusions.getElementsByTagName('tr').length === 0)
         return;
@@ -181,29 +208,29 @@ function del_infusion() {
 }
 
 function update_param() {
-    update_infusions(document.getElementById('rests').children);
+    update_infusions(get_mash_rests());
 }
 
 function update_grain_loss() {
     const LOSS_FACTOR = 1.04; /* L lost/kg of grain, according to BYO and Internet sources */
     
-    set_footer_row('grain_loss', get_config('total_grain')*LOSS_FACTOR);
+    set_footer_row(keys.GRAIN_LOSS, get_config(keys.GRAIN_MASS)*LOSS_FACTOR);
     update_sparge();
 }
 
 function update_total_grain() {
     update_grain_loss();
-    update_infusions(document.getElementById('rests').children);
+    update_infusions(get_mash_rests());
 }
 
 function update_evap() {
     var boil_len, evap_rate, boil_loss;
     
-    boil_len = get_config('boil_len');
-    evap_rate = get_config('evap_rate');
+    boil_len = get_config(keys.BOIL_LEN);
+    evap_rate = get_config(keys.EVAP_RATE);
     boil_loss = (boil_len/60)*evap_rate;
     
-    set_footer_row('boil_loss', boil_loss);
+    set_footer_row(keys.BOIL_LOSS, boil_loss);
     update_runoff();
 }
 
@@ -211,58 +238,58 @@ function update_runoff() {
     const SHRINKAGE_COEFF = 0.96;
     var boil_loss, bat_vol, trub_loss;
     
-    boil_loss = get_footer_vol('boil_loss');
-    bat_vol = get_config('bat_vol');
-    trub_loss = get_config('trub_loss');
+    boil_loss = get_footer_vol(keys.BOIL_LOSS);
+    bat_vol = get_config(keys.BATCH_VOL);
+    trub_loss = get_config(keys.TRUB_LOSS);
     
-    set_footer_row('runoff_vol', (bat_vol/SHRINKAGE_COEFF)+boil_loss+trub_loss);
+    set_footer_row(keys.RUNOFF_VOL, (bat_vol/SHRINKAGE_COEFF)+boil_loss+trub_loss);
     update_sparge();
 }
 
 function update_sparge() {
     var runoff, grain_loss, eq_loss, infusion, total_vol_to_tun;
     
-    runoff = get_footer_vol('runoff_vol');
-    grain_loss = get_footer_vol('grain_loss');
-    eq_loss = get_config('eq_loss');
-    infusion = get_footer_vol('total_infusion');
+    runoff = get_footer_vol(keys.RUNOFF_VOL);
+    grain_loss = get_footer_vol(keys.GRAIN_LOSS);
+    eq_loss = get_config(keys.EQUIP_LOSS);
+    infusion = get_footer_vol(keys.TOTAL_INFUSION_VOL);
     
     total_vol_to_tun = runoff+grain_loss+eq_loss;
     
-    set_footer_row('sparge', total_vol_to_tun-infusion);
+    set_footer_row(keys.SPARGE_VOL, total_vol_to_tun-infusion);
     update_total_mash();
 }
 
 function update_total_mash() {
     var infusion, sparge;
     
-    infusion = get_footer_vol('total_infusion');
-    sparge = get_footer_vol('sparge');
+    infusion = get_footer_vol(keys.TOTAL_INFUSION_VOL);
+    sparge = get_footer_vol(keys.SPARGE_VOL);
     
-    set_footer_row('total_mash', infusion+sparge);
+    set_footer_row(keys.TOTAL_MASH_WATER_VOL, infusion+sparge);
 }
 
 function update_total_vol() {
     const GRAIN_DISPLACEMENT = 0.67; /* L/kg displaced, according to BYO */
     var grain, infusion, tun_vol, total_vol;
     
-    grain = get_config('total_grain');
-    infusion = get_footer_vol('total_infusion');
-    tun_vol = get_config('tun_vol');
+    grain = get_config(keys.GRAIN_MASS);
+    infusion = get_footer_vol(keys.TOTAL_INFUSION_VOL);
+    tun_vol = get_config(keys.MASH_TUN_VOL);
     total_vol = infusion+(grain*GRAIN_DISPLACEMENT);
     
-    set_footer_row('total_vol', total_vol);
-    set_footer_good('total_vol', total_vol<tun_vol);
+    set_footer_row(keys.TOTAL_MASH_VOL, total_vol);
+    set_footer_good(keys.TOTAL_MASH_VOL, total_vol<tun_vol);
 }
 
 function init_config(cfg) {
-    set_config('wg_ratio', cfg.mash_thickness);
-    set_config('t0', cfg.grain_temp);
-    set_config('bat_vol', cfg.batch_volume);
-    set_config('evap_rate', cfg.evaporation_rate);
-    set_config('eq_loss', cfg.equipment_loss);
-    set_config('trub_loss', cfg.trub_loss);
-    set_config('tun_vol', cfg.mash_tun_volume);
+    set_config(keys.MASH_THICKNESS, cfg.mash_thickness);
+    set_config(keys.GRAIN_TEMP, cfg.grain_temp);
+    set_config(keys.BATCH_VOL, cfg.batch_volume);
+    set_config(keys.EVAP_RATE, cfg.evaporation_rate);
+    set_config(keys.EQUIP_LOSS, cfg.equipment_loss);
+    set_config(keys.TRUB_LOSS, cfg.trub_loss);
+    set_config(keys.MASH_TUN_VOL, cfg.mash_tun_volume);
 }
 
 function init() {
